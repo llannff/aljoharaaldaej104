68a1f5ac6b06304c3dcc1962fd0e168cfd9d1c62
llannff <165320469+llannff@users.noreply.github.com>
2024-03-28 22:02:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ YourNAme - Your class name
  #---------------------------
  def on_pin_pressed_p0():
      if input.temperature() > 0:
+         basic.show_string("+")
+         elif input.temperature() < 0:
+             basic.show_string("+")
+             else: 
+                 basic.show_string("Zero")
+                 input.on_pin_pressed(TouchPin.P0, on_pin_presses_p0)
