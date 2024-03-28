60d38207f5158451ecaedd36cf71353bfbfcab80
llannff <165320469+llannff@users.noreply.github.com>
2024-03-28 21:58:57 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 item = 0
 rain: game.LedSprite = None
 """
-YourNAme - Your class name 
+YourNAme - Your class name
+"""
+ #---------------------------
+ def on_pin_pressed_p0():
+     if input.temperature() > 0:
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 let item = 0
 let rain : game.LedSprite = null
-/**  */
+/** YourNAme - Your class name */
